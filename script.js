030e939f6faaa476fd5481125070fc74f74b7112
marlondck <15050029+marlondck@users.noreply.github.com>
2021-11-02 12:03:55 -0400

    modal

---
+++ script.js
@@ -0,0 +1,15 @@
+const modalOverlay = document.querySelector('.modal-overlay')
+const cards = document.querySelectorAll('.card')
+
+
+for (let card of cards) {
+  console.log(card.addEventListener('click', () => {
+    modalOverlay.classList.add('active')
+  }))
+}
+
+document.querySelector('.close-modal').addEventListener('click', () => {
+  modalOverlay.classList.remove('active')
+})
+
+document.querySelector('iframe').src= "https://www.youtube.com/embed/q-J3z58EBfQ"
